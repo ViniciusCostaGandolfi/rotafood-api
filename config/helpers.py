8247dcfc7cfe856fb31d00992bda739a4527887f
Vinicius Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2024-03-12 16:45:25 -0300

    Cria Piginable

--- config/helpers.py
+++ config/helpers.py
@@ -1,4 +1,6 @@
+from typing import List, Optional
 import os
+from typing import Generic, TypeVar
 from pydantic import BaseModel, ConfigDict
 
 
@@ -13,5 +15,33 @@ class BaseModelCamel(BaseModel):
         populate_by_name=True,
         use_enum_values=True)
     
+    
+T = TypeVar("T")
+
+class Paginable(BaseModelCamel, Generic[T]):
+    current_page: int
+    total_pages: int
+    page_size: int
+    total_count: int
+    data: List[T]
+    previous_page: Optional[int] = None
+    next_page: Optional[int] = None
+
+def paginate(items: List[T], page: int, page_size: int) -> Paginable[T]:
+    total_count = len(items)
+    total_pages = (total_count - 1) // page_size + 1
+    
+    previous_page = max(1, page - 1) if page > 1 else None
+    next_page = page + 1 if page < total_pages else None
+
+    return Paginable(
+        current_page=page,
+        total_pages=total_pages,
+        page_size=page_size,
+        total_count=total_count,
+        data=items,
+        previous_page=previous_page,
+        next_page=next_page
+    )
 
 url = os.getenv('TEST_ROTAFOOD_MS_ROUTES_URL') if os.getenv('ENVMODE') == "DEVELOP" else os.getenv('ROTAFOOD_MS_ROUTES_URL')
