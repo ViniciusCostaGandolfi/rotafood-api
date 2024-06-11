2860d35f92522d7bfad0fee46ae3b457c4391aeb
Vinicius Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2024-06-11 19:46:32 -0300

    Cria dtos para catalog

--- api/domain/catalog/dtos/catalog_dto.py
+++ api/domain/catalog/dtos/catalog_dto.py
@@ -2,12 +2,17 @@ from datetime import datetime
 from typing import List, Optional
 from api.config.custom_model import CustomModel
 from api.domain.catalog.dtos.category_dto import CategoryDto
-from api.domain.catalog.dtos.item_dto import ItemDto
+from api.domain.catalog.models.catalog_context_modifier import CatalogContextModifier
     
     
 class CatalogDto(CustomModel):
     id: Optional[str] = None
     name: str
     modified_at: datetime
-    catalog_context_modifier: str
+    catalog_context_modifier: CatalogContextModifier
     categories: List[CategoryDto]
+    
+class CatalogUpdateDto(CustomModel):
+    id: Optional[str] = None
+    name: str
+    catalog_context_modifier: str
