8ccc9f67ae2adddd5482e3336aedba650faf8bb9
Vinicius Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2024-06-11 19:46:46 -0300

    Cria dtos para category

--- api/domain/catalog/dtos/category_dto.py
+++ api/domain/catalog/dtos/category_dto.py
@@ -1,10 +1,20 @@
 from typing import List, Optional
 from api.config.custom_model import CustomModel
 from api.domain.catalog.dtos.item_dto import ItemDto
+from api.domain.catalog.models.category_template import CategoryTemplate
+from api.domain.catalog.models.status import Status
 
 
 class CategoryDto(CustomModel):
     id: Optional[str] = None
-    modified_at: str
-    catalog_context_modifier: str
+    name: str
+    index: int
+    status: Status
+    template: CategoryTemplate
     items: List[ItemDto]
+    
+class CategoryUpdateDto(CustomModel):
+    name: str
+    index: int
+    status: Status
+    template: CategoryTemplate
